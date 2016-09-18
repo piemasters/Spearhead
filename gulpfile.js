239db3228ac99fc0a991c1bb0e30cecef8453265
David Norton <pie_masters@hotmail.co.uk>
2016-09-18 19:24:45 +0100

    simplify gulpfile

--- gulpfile.js
+++ gulpfile.js
@@ -6,17 +6,8 @@
 'use strict';
 
 var gulp = require('gulp');
-var wrench = require('wrench');
-
-/**
- *  This will load all js or coffee files in the gulp directory
- *  in order to load all gulp tasks
- */
-wrench.readdirSyncRecursive('./gulp').filter(function(file) {
-  return (/\.(js|coffee)$/i).test(file);
-}).map(function(file) {
-  require('./gulp/' + file);
-});
+var requireDir = require('require-dir');
+requireDir('./gulp');
 
 
 /**
